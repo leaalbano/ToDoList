09e66ec494fbac234444f8ddc51e90a92d81f321
Lea Albano <leaalbano@csu.fullerton.edu>
2023-10-25 11:34:35 -0700

    Created frontend portion for the todo list

---
+++ src/ToDoList.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import './styles.css'; // Import your CSS file
+
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() === '') {
+      return;
+    }
+    setTasks([...tasks, newTask]);
+    setNewTask('');
+  };
+
+  const markTaskComplete = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index] = 'checked';
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <div className="header">
+        <h2 style={{ margin: '5px' }}>My To Do List</h2>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Title..."
+        />
+        <span onClick={addTask} className="addBtn">
+          Add
+        </span>
+      </div>
+
+      <ul id="myUL">
+        {tasks.map((task, index) => (
+          <li
+            key={index}
+            className={task === 'checked' ? 'checked' : ''}
+            onClick={() => markTaskComplete(index)}
+          >
+            {task}
+            <span
+              className="close"
+              onClick={() => deleteTask(index)}
+            >
+              &#215;
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.jsx', // Make sure the entry file is 'app.jsx'
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    port: 8080, // You can change this port to any available port
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
