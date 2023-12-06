6f30f6e11356564cd839671ab61e2ba7ae147a12
Gilgamesh <32211128+Dontmindmes@users.noreply.github.com>
2023-12-06 15:02:12 -0800

    Update main.go

--- Backend/main.go
+++ Backend/main.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
 	"github.com/rs/cors"
@@ -13,7 +15,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
 
 type Task struct {
 	ID        string    `json:"id,omitempty" bson:"_id,omitempty"`
@@ -27,80 +28,79 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer client.Disconnect(ctx)
 
-	//Configure CORS
-	corsHandler := cors.New(cors.Options{
-        AllowedOrigins: []string{"http://localhost:8080"}, 
-        AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
-    })
+	router := setupRouter(client)
+	server := &http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
 
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
 
-	// Proceed with setting up your API routes
-    http.Handle("/createTask", corsHandler.Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        createTask(client, w, r)
-    })))
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutting down server...")
 
-    http.Handle("/getTasks", corsHandler.Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        getTasks(client, w, r)
-    })))
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
 
-    http.Handle("/deleteTask", corsHandler.Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        deleteTask(client, w, r)
-    })))
-
-    http.Handle("/updateTask", corsHandler.Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        updateTask(client, w, r)
-    })))
-
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Println("Server exiting")
 }
 
-// UpdateTask function
-func updateTask(client *mongo.Client, w http.ResponseWriter, r *http.Request) {
+func setupRouter(client *mongo.Client) *http.ServeMux {
+	router := http.NewServeMux()
+
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:8080"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
+	})
+
+	router.Handle("/createTask", corsHandler.Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		createTask(client, w, r)
+	})))
+	router.Handle("/getTasks", corsHandler.Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		getTasks(client, w, r)
+	})))
+	router.Handle("/deleteTask", corsHandler.Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		deleteTask(client, w, r)
+	})))
+	router.Handle("/updateTask", corsHandler.Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		updateTask(client, w, r)
+	})))
+
+	return router
+}
+
+func createTask(client *mongo.Client, w http.ResponseWriter, r *http.Request) {
 	var task Task
-	err := json.NewDecoder(r.Body).Decode(&task)
-	if err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	// Check if the task ID is provided
-	if task.ID == "" {
-		http.Error(w, "Missing task ID", http.StatusBadRequest)
-		return
-	}
+	task.CreatedAt = time.Now()
 
-	// Convert the task ID to an ObjectID
-	objectID, err := primitive.ObjectIDFromHex(task.ID)
-	if err != nil {
-		http.Error(w, "Invalid task ID", http.StatusBadRequest)
-		return
-	}
-
-	// Connect to the collection
 	collection := client.Database("todoDB").Collection("tasks")
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	// Create a filter for the document to update
-	filter := bson.M{"_id": objectID}
-
-	// Define the update
-	update := bson.M{
-		"$set": bson.M{
-			"title":  task.Title,
-			"status": task.Status,
-			// You can add other fields here that you want to update
-		},
-	}
-
-	// Update the task
-	result, err := collection.UpdateOne(ctx, filter, update)
+	result, err := collection.InsertOne(ctx, task)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -109,46 +109,53 @@ func updateTask(client *mongo.Client, w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(result)
 }
 
-// Import necessary packages like "net/http", "encoding/json", etc.
+func getTasks(client *mongo.Client, w http.ResponseWriter, r *http.Request) {
+	var tasks []Task
+	collection := client.Database("todoDB").Collection("tasks")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
 
-func createTask(client *mongo.Client, w http.ResponseWriter, r *http.Request) {
-	var task Task
-	err := json.NewDecoder(r.Body).Decode(&task)
+	cursor, err := collection.Find(ctx, bson.M{})
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer cursor.Close(ctx)
+
+	for cursor.Next(ctx) {
+		var task Task
+		if err := cursor.Decode(&task); err != nil {
+			log.Println("Error decoding task:", err)
+			continue
+		}
+		tasks = append(tasks, task)
+	}
+
+	if err := cursor.Err(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	// Set the CreatedAt field to the current time
-	task.CreatedAt = time.Now()
-
-	collection := client.Database("todoDB").Collection("tasks")
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-	result, _ := collection.InsertOne(ctx, task)
-
-	json.NewEncoder(w).Encode(result)
+	json.NewEncoder(w).Encode(tasks)
 }
 
 func deleteTask(client *mongo.Client, w http.ResponseWriter, r *http.Request) {
-	// Get the task ID from URL query parameter
 	taskID := r.URL.Query().Get("id")
 	if taskID == "" {
 		http.Error(w, "Missing task ID", http.StatusBadRequest)
 		return
 	}
 
-	// Convert the taskID string to an ObjectId type
 	objectID, err := primitive.ObjectIDFromHex(taskID)
 	if err != nil {
 		http.Error(w, "Invalid task ID", http.StatusBadRequest)
 		return
 	}
 
-	// Connect to the collection
 	collection := client.Database("todoDB").Collection("tasks")
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	// Delete the task
 	result, err := collection.DeleteOne(ctx, bson.M{"_id": objectID})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -158,23 +165,41 @@ func deleteTask(client *mongo.Client, w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(result)
 }
 
+func updateTask(client *mongo.Client, w http.ResponseWriter, r *http.Request) {
+	var task Task
+	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-func getTasks(client *mongo.Client, w http.ResponseWriter, r *http.Request) {
-	var tasks []Task
+	if task.ID == "" {
+		http.Error(w, "Missing task ID", http.StatusBadRequest)
+		return
+	}
+
+	objectID, err := primitive.ObjectIDFromHex(task.ID)
+	if err != nil {
+		http.Error(w, "Invalid task ID", http.StatusBadRequest)
+		return
+	}
+
 	collection := client.Database("todoDB").Collection("tasks")
-	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	cursor, err := collection.Find(ctx, bson.M{})
+	filter := bson.M{"_id": objectID}
+	update := bson.M{
+		"$set": bson.M{
+			"title":  task.Title,
+			"status": task.Status,
+		},
+	}
+
+	result, err := collection.UpdateOne(ctx, filter, update)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	defer cursor.Close(ctx)
-	for cursor.Next(ctx) {
-		var task Task
-		cursor.Decode(&task)
-		tasks = append(tasks, task)
-	}
 
-	json.NewEncoder(w).Encode(tasks)
+	json.NewEncoder(w).Encode(result)
 }
